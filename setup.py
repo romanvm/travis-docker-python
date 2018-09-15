cc3c7aa995e1fd1c80ed5c9503a83712f26e99ed
Roman Miroshnychenko <roman1972@gmail.com>
2018-09-15 21:43:21 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="fibonacci",
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="Fibonacci numbers",
+    py_modules=['fibonacci']
+)
---
+++ test.py
@@ -0,0 +1,11 @@
+from fibonacci import fib
+
+#0, 1, 1, 2, 3, 5, 8
+def test_fib():
+    assert fib(0) == 0
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(3) == 2
+    assert fib(4) == 3
+    assert fib(5) == 5
+    assert fib(6) == 8
